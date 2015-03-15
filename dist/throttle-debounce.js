3b85a8dba946435996e3dbc1c429b41474efa51e
Ivan Nikolić <niksy5@gmail.com>
2015-03-15 16:30:40 +0100

    Release 0.1.1

--- dist/throttle-debounce.js
+++ dist/throttle-debounce.js
@@ -1,4 +1,4 @@
-/*! throttle-debounce 0.1.0 - Throttle/debounce your functions. | Author: Ivan Nikolić <niksy5@gmail.com> (http://ivannikolic.com/), 2014 | License: MIT | Original author: Ben Alman (http://benalman.com) | Original license: Dual licensed under the MIT and GPL licenses */
+/*! throttle-debounce 0.1.1 - Throttle/debounce your functions. | Author: Ivan Nikolić <niksy5@gmail.com> (http://ivannikolic.com/), 2015 | License: MIT | Original author: Ben Alman (http://benalman.com) | Original license: Dual licensed under the MIT and GPL licenses */
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var throttle = require(4);
 
