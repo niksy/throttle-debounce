11c9a4faad97a133c13a9f1960c7a368a865ed66
Ted Wu <ted.wu@linecorp.com>
2018-04-19 09:15:50 +0900

    add cancel for debounce

--- throttle.js
+++ throttle.js
@@ -97,6 +97,10 @@ export default function ( delay, noTrailing, callback, debounceMode ) {
 
 	}
 
+	wrapper.cancel = function () {
+		clearTimeout(timeoutID);
+	}
+
 	// Return the wrapper function.
 	return wrapper;
 
