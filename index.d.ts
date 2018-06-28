a5420bfffc614c7675bf1975ee79031b07d7ec6b
Frank Li <frankvsense@gmail.com>
2018-06-28 12:30:33 -0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,12 @@
-declare module 'throttle-debounce' {
-  export declare function throttle<T extends (...args: any[]) => any>(
-    delay: number,
-    noTrailing: boolean,
-    callback?: T,
-    debounceMode?: boolean
-  );
+export declare function throttle<T extends (...args: any[]) => any>(
+  delay: number,
+  noTrailing: boolean,
+  callback?: T,
+  debounceMode?: boolean
+);
 
-  export declare function debounce<T extends (...args: any[]) => any>(
-    delay: number,
-    atBegin: boolean,
-    callback?: T
-  );
-}
+export declare function debounce<T extends (...args: any[]) => any>(
+  delay: number,
+  atBegin: boolean,
+  callback?: T
+);
